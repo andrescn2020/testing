a05c100a88a84d6dfb43d0e3f2118f9dfa0fd8b9
Andres <andrescn_2010@hotmail.com>
2023-01-03 11:29:59 -0300

    Intro

---
+++ src/__tests__/example_globals.test.js
@@ -0,0 +1,11 @@
+describe('this is for group test cases', () => {
+    test('description', () => {
+        expect(true).toBe(true);
+    });
+    test('description2', () => {
+        expect(true).toBe(true);
+    });
+    test('description3', () => {
+        expect(true).toBe(true);
+    });
+})
